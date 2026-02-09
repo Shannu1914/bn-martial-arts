caeaa92cc2be1271a56e1a94916cd1307fa8dd08
Lovelyshannu <vasamshettyshanmukh@gmail.com>
2026-02-10 01:12:01 +0530

    Update API base URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.REACT_APP_API_BASE || 'https://bn-martial-arts.onrender.com';
+const API_BASE = 'https://bn-martial-arts.onrender.com';
 
 export async function apiFetch(path, opts = {}) {
   const res = await fetch(`${API_BASE}${path}`, opts);
